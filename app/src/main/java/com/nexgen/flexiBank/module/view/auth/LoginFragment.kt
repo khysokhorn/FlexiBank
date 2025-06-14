1db9457dea37513fba93ca42a4eb69e4116a7d77
sokkhorn <sokkhorn169.infinity@gmail.com>
2025-06-14 21:11:13 +0700

    Add auth screen

--- app/src/main/java/com/nexgen/flexiBank/module/view/auth/LoginFragment.kt
+++ app/src/main/java/com/nexgen/flexiBank/module/view/auth/LoginFragment.kt
@@ -2,10 +2,12 @@ package com.nexgen.flexiBank.module.view.auth
 
 import android.os.Bundle
 import android.text.Html
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.nexgen.flexiBank.R
 import com.nexgen.flexiBank.databinding.FragmentLoginBinding
 
 /**
@@ -22,7 +24,6 @@ class LoginFragment : Fragment() {
 
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -30,6 +31,10 @@ class LoginFragment : Fragment() {
         val htmlText =
             "By pressing <b>\"Sign Up\"</b> you accept our <font color='blue'>privacy policy</font> and <font color='blue'>terms & Conditions</font>. Your information will be securely " + "encrypted.";
         binding.txtTermCondition.text = Html.fromHtml(htmlText, Html.FROM_HTML_MODE_LEGACY);
+        binding.btnSignUp.setOnClickListener {
+            val navigation = findNavController()
+            navigation.navigate(R.id.action_LoginFragment_to_OtpChoiceFragment)
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/nexgen/flexiBank/module/view/auth/OtpChoiceFragment.kt
+++ app/src/main/java/com/nexgen/flexiBank/module/view/auth/OtpChoiceFragment.kt
@@ -1,20 +1,20 @@
 package com.nexgen.flexiBank.module.view.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.nexgen.flexiBank.R
-import com.nexgen.flexiBank.databinding.FragmentRegisterBinding
+import com.nexgen.flexiBank.databinding.FragmentOtpChoiceBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class RegisterFragment : Fragment() {
+class OtpChoiceFragment : Fragment() {
 
-    private var _binding: FragmentRegisterBinding? = null
+    private var _binding: FragmentOtpChoiceBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,15 +24,17 @@ class RegisterFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
-        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        _binding = FragmentOtpChoiceBinding.inflate(inflater, container, false)
         return binding.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.email.setOnClickListener {
+            val navigaton = findNavController()
+            navigaton.navigate(R.id.action_OtpChoiceFragment_to_OtpVerifyFragment)
+        }
     }
 
     override fun onDestroyView() {
