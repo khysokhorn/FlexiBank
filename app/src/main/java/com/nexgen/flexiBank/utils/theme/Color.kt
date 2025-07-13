21a0f4f8830a666a7f76c644d9e534b3b809538f
sokkhorn <sokkhorn169.infinity@gmail.com>
2025-07-13 11:07:37 +0700

    + Init android compose

---
+++ app/src/main/java/com/nexgen/flexiBank/utils/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.nexgen.flexiBank.utils.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Dark theme colors
+val Blue = Color(0xFF5260FE)
+val GradientDarkGreen = Color(0xFF6E7AFF)
+val BlueLight3 = Color(0xFF8C96FF)
+val BlueLight2 = Color(0xFFAEB5FF)
+val BlueLight1 = Color(0xFFD4D8FF)
+
+// Light theme colors
+val BackgroundColor = Color(0xFFF0F0F0)
+val BorderColor = Color(0xFFD3D3D3)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val Gray400 = Color(0xFFBDBDBD)
+val Gray600 = Color(0xFF757575)
+val Hint = Color(0xFF7B7E82)
