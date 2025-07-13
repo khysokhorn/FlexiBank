de2d95adbb74e8584e285c34918c83a8572ee072
sokkhorn <sokkhorn169.infinity@gmail.com>
2025-07-13 10:44:40 +0700

    + Add action select on document type

--- app/src/main/java/com/nexgen/flexiBank/module/view/auth/fragment/IdentityVerifyFragment.kt
+++ app/src/main/java/com/nexgen/flexiBank/module/view/auth/fragment/IdentityVerifyFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.nexgen.flexiBank.R
 import com.nexgen.flexiBank.databinding.FragmentIdentityVerifyBinding
 import com.nexgen.flexiBank.module.view.base.BaseFragment
 import com.nexgen.flexiBank.module.view.verifyDocument.VerifyDocumentActivity
@@ -16,7 +17,8 @@ import com.nexgen.flexiBank.viewmodel.RegisterViewModel
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class IdentityVerifyFragment : BaseFragment<RegisterViewModel, FragmentIdentityVerifyBinding, AppRepository>() {
+class IdentityVerifyFragment :
+    BaseFragment<RegisterViewModel, FragmentIdentityVerifyBinding, AppRepository>() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -31,9 +33,14 @@ class IdentityVerifyFragment : BaseFragment<RegisterViewModel, FragmentIdentityV
 
     override fun getViewModel(): Class<RegisterViewModel> = RegisterViewModel::class.java
 
-    override fun getFragmentBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentIdentityVerifyBinding = FragmentIdentityVerifyBinding.inflate(layoutInflater, container, false)
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentIdentityVerifyBinding =
+        FragmentIdentityVerifyBinding.inflate(layoutInflater, container, false)
 
-    override fun getRepository(): AppRepository = AppRepository(remoteDataSource.buildApi(requireActivity(), ApiInterface::class.java))
+    override fun getRepository(): AppRepository =
+        AppRepository(remoteDataSource.buildApi(requireActivity(), ApiInterface::class.java))
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -41,6 +48,23 @@ class IdentityVerifyFragment : BaseFragment<RegisterViewModel, FragmentIdentityV
             val intent = Intent(requireContext(), VerifyDocumentActivity::class.java)
             startActivity(intent)
         }
+
+        binding.nid.setOnClickListener {
+            updateDocumentTypeSelection(binding.nid.id, isNidSelected = true)
+        }
+        binding.passport.setOnClickListener {
+            updateDocumentTypeSelection(binding.passport.id, isNidSelected = false)
+        }
+    }
+
+    private fun updateDocumentTypeSelection(selectedId: Int, isNidSelected: Boolean) {
+        viewModel.documentTypeOnClick(selectedId)
+
+        binding.nid.setBackgroundResource(if (isNidSelected) R.drawable.rounded_border else R.drawable.rounded_background)
+        binding.imgNidSelected.setImageResource(if (isNidSelected) R.drawable.circle_active_drawable else R.drawable.circle_drawable)
+
+        binding.passport.setBackgroundResource(if (!isNidSelected) R.drawable.rounded_border else R.drawable.rounded_background)
+        binding.imgPassportSelected.setImageResource(if (!isNidSelected) R.drawable.circle_active_drawable else R.drawable.circle_drawable)
     }
 
     companion object {
--- app/src/main/java/com/nexgen/flexiBank/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/nexgen/flexiBank/viewmodel/RegisterViewModel.kt
@@ -4,4 +4,9 @@ import androidx.lifecycle.ViewModel
 import com.nexgen.flexiBank.repository.AppRepository
 
 class RegisterViewModel(private val repository: AppRepository) : ViewModel() {
+    private var selectedId: Int = 0
+    fun documentTypeOnClick(id: Int) {
+        selectedId = id
+    }
+    fun getSelectedId(): Int = selectedId
 }
