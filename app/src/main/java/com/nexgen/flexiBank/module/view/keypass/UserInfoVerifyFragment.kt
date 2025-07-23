0dc1ef103cc65c0dd4c02fa9b3c3a1aebeb5b261
sokkhorn <sokkhorn169.infinity@gmail.com>
2025-07-23 23:04:49 +0700

    + Add motion layout animation to get start screen

--- app/src/main/java/com/nexgen/flexiBank/module/view/keypass/UserInfoVerifyFragment.kt
+++ app/src/main/java/com/nexgen/flexiBank/module/view/keypass/UserInfoVerifyFragment.kt
@@ -79,7 +79,7 @@ class UserInfoVerifyFragment : BaseComposeFragment<PasscodeViewModel, AppReposit
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             ScrollableNumberPickerWithIndicator(
-                range = 0..99,
+                range = 0..10,
                 initialValue = 0,
                 onCountCompleted = { viewModel.onVerificationComplete() }
             )
@@ -109,7 +109,7 @@ class UserInfoVerifyFragment : BaseComposeFragment<PasscodeViewModel, AppReposit
         val progressAngle by animateFloatAsState(
             targetValue = targetAngle,
             animationSpec = tween(
-                durationMillis = 10000,
+                durationMillis = 100,
                 easing = LinearEasing
             ),
             label = "progress arc",
