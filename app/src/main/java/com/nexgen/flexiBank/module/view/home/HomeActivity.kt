22fe16a1a4cc27925f9c93e6380ca4ad370db14a
sokkhorn <sokkhorn169.infinity@gmail.com>
2025-07-24 13:31:20 +0700

    Init image circle with Glide

--- app/src/main/java/com/nexgen/flexiBank/module/view/home/HomeActivity.kt
+++ app/src/main/java/com/nexgen/flexiBank/module/view/home/HomeActivity.kt
@@ -1,7 +1,7 @@
 package com.nexgen.flexiBank.module.view.home
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import com.bumptech.glide.Glide
 import com.nexgen.flexiBank.databinding.ActivityHomeBinding
 import com.nexgen.flexiBank.module.view.base.BaseMainActivity
 import com.nexgen.flexiBank.module.view.home.viewModel.HomeViewModel
@@ -9,9 +9,14 @@ import com.nexgen.flexiBank.network.ApiInterface
 import com.nexgen.flexiBank.repository.AppRepository
 
 class HomeActivity : BaseMainActivity<HomeViewModel, ActivityHomeBinding, AppRepository>() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Glide.with(this)
+            .load("https://img.freepik.com/premium-psd/stylish-young-man-3d-icon-avatar-people_431668-1607.jpg")
+            .circleCrop()
+            .into(binding.imgProfile)
     }
+
     override fun getViewModel(): Class<HomeViewModel> = HomeViewModel::class.java
 
     override fun getActivityBinding(): ActivityHomeBinding =
